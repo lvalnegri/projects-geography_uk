018c7cd3b24f5060511adb4878ef359aa5109323
Luca Valnegri <Luca Valnegri>
2017-04-09 23:43:36 +0100

    first commit

---
+++ 20-locations.R
@@ -0,0 +1,29 @@
+###############################################################
+# 20- LOCATIONS
+###############################################################
+
+### 1- Load packages, Set variables ---------------------------------------------------------------------------------------------
+pkg <- c('data.table', 'RMySQL')
+pkg <- lapply(pkg, require, character.only = TRUE)
+data.path <- 
+    if(substr(Sys.info()['sysname'], 1, 1) == 'W'){
+        'D:/cloud/OneDrive/data/UK/geography/postcodes/'
+    } else {
+        
+    }
+
+### 2- Load data ----------------------------------------------------------------------------------------------------------------
+db_conn <- dbConnect(MySQL(), group = 'homeserver', dbname = 'geographyUK')
+
+
+
+### - SAVE RESULTS -------------------------------------------------------------------------------------------------------------
+dbSendQuery(db_conn, "TRUNCATE TABLE postcodes")
+dbWriteTable(db_conn, 'postcodes', postcodes, row.names = FALSE, append = TRUE)
+
+
+### - CLEAN & EXIT -------------------------------------------------------------------------------------------------------------
+dbDisconnect(db_conn)
+rm(list = ls())
+gc()
+
---
+++ 70-spatial_statistics.R
@@ -0,0 +1,27 @@
+###############################################################
+# 70- SPATIAL STATISTICS
+###############################################################
+
+### 1- Load packages, Set variables ---------------------------------------------------------------------------------------------
+pkg <- c('data.table', 'rgdal', 'RMySQL')
+pkg <- lapply(pkg, require, character.only = TRUE)
+data.path <- 
+    if(substr(Sys.info()['sysname'], 1, 1) == 'W'){
+        'D:/cloud/OneDrive/data/UK/geography/postcodes/'
+    } else {
+        
+    }
+
+### 2- Load data ----------------------------------------------------------------------------------------------------------------
+db_conn <- dbConnect(MySQL(), group = 'homeserver', dbname = 'geographyUK')
+strSQL = "
+"
+dataset <- data.table(dbGetQuery(db_conn, strSQL), key = '')
+dataset <- data.table(dbReadTable(db_conn, 'postcodes'), key = '')
+
+
+### - CLEAN & EXIT -------------------------------------------------------------------------------------------------------------
+dbDisconnect(db_conn)
+rm(list = ls())
+gc()
+
