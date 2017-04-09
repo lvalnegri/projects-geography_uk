3997e77c237f560760f8648a93babd8569ff5b04
Luca <l.valnegri@datamaps.co.uk>
2017-04-09 13:27:39 +0100

    Update 10-postcodes.R

--- 10-postcodes.R
+++ 10-postcodes.R
@@ -1,11 +1,3 @@
-# There are two files, NHSPD and ONSPD, with overlapping information.
-# Both contain ALL:
-# - current (live) postcodes within the UK, CI and IM, as received monthly from Royal Mail.
-# - terminated (closed) postcodes that have NOT been subsequently re-used by Royal Mail within the UK, and by the postal admini in CI and IM
-# We'll be using only the shorter 7-char *PCD* form: 
-# - outward code as 2/3/4 chars, left aligned (3rd and 4th char may be blank)
-# - inward code always 3 chars (1st numeric, 2nd and 3rd alpha) – right aligned;
-
 ### 1- Load packages, Set variables ------------------------------------------------------------------------------------------
 library('data.table')
 library('RMySQL')
