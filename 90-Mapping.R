b75f40b6fdb2343420d2593e237833f31f998eae
luca valnegri <l.valnegri@datamaps.co.uk>
2018-02-19 14:27:24 +0000

    - added starting code for plotting point

--- 90-Mapping.R
+++ 90-Mapping.R
@@ -11,10 +11,9 @@
 
 
 
-plot.boundaries <- function(type, parent.type = NA, parent.code = NA){
-    library(rgdal)
-    library(leaflet)
-    library(htmltools)
+plot_boundaries <- function(type, parent.type = NA, parent.code = NA){
+    pkg <- c('rgdal', 'leaflet', 'htmltools')
+    invisible(lapply(pkg, require,  char = TRUE))
     shp <- readOGR(boundaries.path, type)
     if(!is.na(type)){
         db_conn <- dbConnect(MySQL(), group = 'local', dbname = 'geographyUK')
@@ -52,3 +51,33 @@ plot.boundaries <- function(type, parent.type = NA, parent.code = NA){
             )
         )
 }
+
+
+plot_points <- function(loca = 'OA', parent_id = NULL, parent_code = NULL, var_colour = NULL, var_size = NULL, var_facet = NULL){
+    pkg <- c('ggmap', 'ggplot2')
+    invisible(lapply(pkg, require,  char = TRUE))
+    dbc <- dbConnect(MySQL(), group = 'dataOps', dbname = 'geography_uk')
+    if(loca == 'OA'){
+        strSQL <- "SELECT OA, wx_lon, wy_lat"
+        if(!is.null(var_facet)) strSQL <- paste(strSQL, ', ', var_facet, ' AS ZF')
+        strSQL <- paste(strSQL, "FROM lookups")
+        if(!is.null(parent_id)) strSQL <- paste0(strSQL, " WHERE ", parent_id, " = '", parent_code, "'")
+        pnts <- dbGetQuery(dbc, strSQL)
+        # add auxiliary variables
+        # if(!is.null(var_colour)) strSQL <- paste(strSQL, ', ', var_colour, ' AS ZC')
+        # if(!is.null(var_size)) strSQL <- paste(strSQL, ', ', var_size, ' AS ZS')
+    } else {
+        pnts <- dbGetQuery(dbc, "SELECT location_id, wx_lon, wy_lat FROM lookups WHERE LEFT(CTRY, 1) != 'N'")
+    }
+    dbDisconnect(dbc)
+    
+    g <- ggplot(pnts, aes(wx_lon, wy_lat)) + 
+            geom_point() + 
+            coord_map()
+    if(!is.null(var_facet)) g <- g + facet_wrap(~ ZF)
+    
+    g
+}
+
+plot_points(parent_id = 'CTY', parent_code = 'E13000001', var_facet = 'LAD')
+
