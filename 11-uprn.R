1cb787eb635280dc6f223d798e9182842c887f38
luca valnegri <l.valnegri@datamaps.co.uk>
2018-02-19 14:25:11 +0000

    - now read only zip file instead of CSVs, deleting everything at the end  - changed data.path in data_path

--- 11-uprn.R
+++ 11-uprn.R
@@ -1,14 +1,15 @@
 ###############################################################
 # 11- UPRN
 ###############################################################
+# download in data_path most recent zip file @ http://geoportal.statistics.gov.uk/datasets?q=ONS%20Address%20Directory&sort=-updatedAt  
+# ===> be sure that's the only file in the directory! <=== 
 
 ### Load packages ---------------------------------------------------------------------------------------------------------------
 pkg <- c('data.table', 'RMySQL')
 pkg <- lapply(pkg, require, character.only = TRUE)
 
 ### Set variables ---------------------------------------------------------------------------------------------------------------
-current_month <- 1709
-data.path <- 
+data_path <- 
     if (substr(Sys.info()['sysname'], 1, 1) == 'W') {
         'D:/cloud/OneDrive/data/UK/geography/uprn'
     } else {
@@ -16,8 +17,17 @@ data.path <-
     }
 dt <- data.table(OA = numeric(0), N = numeric(0))
 
+### read filename and assign current_month variable -----------------------------------------------------------------------------
+fn <- list.files(data_path)
+current_month <- substr(fn, 7, 9)
+current_month <- which(current_month == c('JAN', 'FEB', 'MAR', 'APR', 'MAY', 'JUN', 'JUL', 'AUG', 'SEP', 'OCT', 'NOV', 'DEC'))
+current_month <- paste0(substr(fn, 13, 14), current_month)
+
+### unzip file ------------------------------------------------------------------------------------------------------------------
+unzip(file.path(data_path, fn), exdir = data_path)
+
 ### Loop through files: load OAs only, count records by OA, append to result dataaset -------------------------------------------
-f_names <- dir(data.path, full.names = TRUE)
+f_names <- dir(file.path(data_path, 'Data'), full.names = TRUE)
 n_files <- length(f_names)
 for(idx in 1:n_files){
     print(paste('Reading file', idx, 'out of', n_files))
@@ -51,6 +61,18 @@ if(sql_res) dbSendQuery(dbc, paste0("ALTER TABLE uprns DROP COLUMN D", current_m
 dbSendQuery(dbc, paste0("ALTER TABLE uprns ADD COLUMN D", current_month, " SMALLINT(5) UNSIGNED NULL DEFAULT NULL AFTER oa"))
 dbSendQuery(dbc, paste0("UPDATE uprns u JOIN tmp t ON t.OA = u.OA SET u.D", current_month, " = t.N"))
 
+# check totals by RGN/CTRY with Table 1 in the "ONSUD User Guide"
+strSQL <- "
+    SELECT lk.RGN, lc.name, SUM(tot_uprn) AS c
+    FROM uprns u 
+    	JOIN lookups lk ON lk.OA = u.oa
+    	JOIN locations lc ON lc.location_id = lk.RGN AND lc.type_id = 'RGN'
+    GROUP BY RGN
+"
+t <- dbGetQuery(dbc, strSQL)
+
+# remove files from disk
+unlink(file.path(data_path, '*'), recursive = TRUE)
 
 ### Clean & Exit ----------------------------------------------------------------------------------------------------------------
 dbSendQuery(dbc, "DROP TABLE tmp")
